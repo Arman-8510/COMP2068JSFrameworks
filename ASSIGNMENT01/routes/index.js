93fb35a8edd57ba84c184cf0db426845e0ff1024
Arman-8510 <Armanjot10singh@gmail.com>
2024-10-20 18:49:44 -0400

    first and last

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home page route
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+
+// About page route
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About Me" });
+});
+
+// Projects page route
+router.get("/projects", function (req, res, next) {
+  res.render("projects", { title: "Projects" });
+});
+
+// Contact page route
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact Me" });
 });
 
 module.exports = router;
