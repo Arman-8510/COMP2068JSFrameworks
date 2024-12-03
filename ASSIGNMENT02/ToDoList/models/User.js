bb646ab9a7f63bfc711033bdffce99cefa986f98
Arman-8510 <134649122+Arman-8510@users.noreply.github.com>
2024-12-04 01:23:11 +0530

    Update User.js

--- ASSIGNMENT02/ToDoList/models/User.js
+++ ASSIGNMENT02/ToDoList/models/User.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   githubId: { type: String, required: true },
   username: { type: String, required: true },
-  email: { type: String, required: true }, // Ensure email is required
-  password: { type: String, required: false }, // Make password optional
+  email: { type: String, required: true }, 
+  password: { type: String, required: false }, 
 });
 
 const User = mongoose.model('User', userSchema);
